ec6beec8e16bc255f6fe8702cb525c4c7d1ed522
CharanCodeCraft <charangowak911@gmail.com>
2025-03-10 00:37:46 +0530

    backend-deploy-config

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser')
 var cors = require('cors')
 const dotenv = require('dotenv')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(cors())
